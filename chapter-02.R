1ba9075b5cdfc1fff70d01f65b7e060205559afd
rodferro <rod_ferro@msn.com>
2018-09-18 23:46:56 -0300

    Add "2.2 Suavização exponencial simples (SES)"

--- chapter-02.R
+++ chapter-02.R
@@ -24,3 +24,16 @@ legend(1985, 8000, c("Série Original", "Tendência - Filtro HP"),
        col=c("black", "red"), lwd=c(1, 2), bty="n")
 plot(hpfilter(cafe, type="lambda")$cycle, ylab="Componente Cíclica",
      xlab="Tempo")
+
+# 2.2 Suavização exponencial simples (SES)
+set.seed(1234)
+serie <- ts(runif(100, 10, 15), start=c(1915, 1), frequency=1)
+plot(serie)
+
+ajuste <- HoltWinters(serie, beta=FALSE, gamma=FALSE)
+ajuste
+plot(ajuste, xlab="Tempo", ylab="Valores observados/ajustados", main="")
+
+ajuste_prev <- forecast(ajuste, h=10, level=95)
+ajuste_prev
+plot(ajuste_prev, main="", xlab="Tempo", ylab="Dados")
