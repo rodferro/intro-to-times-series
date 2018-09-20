9185bf5377fbff5d6fb7ac3728d1b8a6985c2722
rodferro <rod_ferro@msn.com>
2018-09-19 23:17:32 -0300

    Add "2.3 Suavização exponencial de Holt (SEH)"

--- chapter-02.R
+++ chapter-02.R
@@ -26,6 +26,7 @@ plot(hpfilter(cafe, type="lambda")$cycle, ylab="Componente Cíclica",
      xlab="Tempo")
 
 # 2.2 Suavização exponencial simples (SES)
+par(mfrow=c(1,1))
 set.seed(1234)
 serie <- ts(runif(100, 10, 15), start=c(1915, 1), frequency=1)
 plot(serie)
@@ -37,3 +38,22 @@ plot(ajuste, xlab="Tempo", ylab="Valores observados/ajustados", main="")
 ajuste_prev <- forecast(ajuste, h=10, level=95)
 ajuste_prev
 plot(ajuste_prev, main="", xlab="Tempo", ylab="Dados")
+
+# 2.3 Suavização exponencial de Holt (SEH)
+dados <- read.csv2("data/consumo_energia_eletrica_regiao_sudeste.csv")
+dados <- dados[-457, ] # retirando janeiro de 2017
+consumo <- ts(dados[, 2], start=c(1979, 1), frequency=12)
+plot(consumo, xlab="Tempo", ylab="Consumo de Energia Elétrica (Gwh)", main="")
+
+ajuste_holt <- HoltWinters(consumo, gamma=FALSE)
+ajuste_holt
+
+plot(consumo, xlab="Tempo", ylab="Valores Observados/Ajustados", main="")
+lines(fitted(ajuste_holt)[, 1], lwd=2, col="red")
+legend(1980, 20000, c("Consumo Energia Elétrica", "Ajuste SEH"),
+       lwd=c(1, 2), col=c("black", "red"), bty="n")
+
+prev_holt <- forecast(ajuste_holt, h=12, level=95)
+prev_holt
+
+plot(prev_holt, xlab="Tempo", ylab="Valores Observados/Previstos", main="")
